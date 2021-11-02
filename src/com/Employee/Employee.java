c1333bab0bc1e426208999afd5b6adaa12080ef7
sneh3195 <snehjainnsk@gmail.com>
2021-11-02 16:00:19 +0530

    Employee wages

--- src/com/Employee/Employee.java
+++ src/com/Employee/Employee.java
@@ -1,13 +1,17 @@
 package com.Employee;
+
 public class Employee {
-	public static void main(String[] args) {
-			double attendance =Math.floor(Math.random()*2);
-                System.out.println(attendance);
-              if(attendance == 1) {
-        	   System.out.println("Employee is present");
-           }
-           else {
-        	   System.out.println("Employee is absent");
+			public static void main(String[] args) {
+				double attendance =Math.floor(Math.random()*2);
+				int wages; 
+                	System.out.println(attendance);
+           if(attendance == 1) {
+        	   		//System.out.println("Employee is present");
+        	   		wages=160;
+        	   		System.out.println("Employee is present \nwages = " + wages);
+           }else {
+        	         wages=0;
+        	   		System.out.println("Employee is absent \nwages = " + wages);
            }
           
 	}
