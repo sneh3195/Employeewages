f86d66ef7be948ba00e1429be27108a04dd08c2c
sneh3195 <snehjainnsk@gmail.com>
2021-11-02 16:08:28 +0530

    Parttime wages

--- src/com/Employee/Employee.java
+++ src/com/Employee/Employee.java
@@ -3,12 +3,20 @@ package com.Employee;
 public class Employee {
 			public static void main(String[] args) {
 				double attendance =Math.floor(Math.random()*2);
+				double partTime =Math.floor(Math.random()*2);
+
 				int wages; 
                 	System.out.println(attendance);
            if(attendance == 1) {
         	   		//System.out.println("Employee is present");
+        	   if(partTime == 1) {
         	   		wages=160;
-        	   		System.out.println("Employee is present \nwages = " + wages);
+        	   		System.out.println("Employee is present & PartTime \nwages = " + wages);
+        	   }
+        	    else {
+        	    	wages=160;
+        	   		System.out.println("Employee is present & FullTime \nwages = " + wages);
+        	    }
            }else {
         	         wages=0;
         	   		System.out.println("Employee is absent \nwages = " + wages);
